53c72c891d3dada6ae79497056a888ffe6af4a2e
Tris Setiawan <tris.setiawan@yahoo.com>
2016-07-27 08:54:02 +0000

    1.0.0 - add route v1/inventories/docs/:module

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ transferInDocRouter.applyRoutes(server);
 var transferOutDocRouter = require('./src/routers/v1/transfer-out-doc-router');
 transferOutDocRouter.applyRoutes(server); 
 
+var inventoryDocModuleRouter = require('./src/routers/v1/inventory-doc-module-router');
+inventoryDocModuleRouter.applyRoutes(server); 
+
 var ModuleRouter = require('./src/routers/v1/module-router');
 ModuleRouter.applyRoutes(server); 
 
