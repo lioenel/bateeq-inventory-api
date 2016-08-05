e0c752249f5edfd8b6d8f8a3fb4a1e6dcc75b20c
danielmoonlay <daniel.nababan@moonlay.com>
2016-08-05 17:17:44 +0700

    api khusus pending SPK

---
+++ src/routers/v1/inventory-receive-module-router.js
@@ -0,0 +1,97 @@
+var Router = require('restify-router').Router;;
+var router = new Router();
+var map = require('bateeq-module').inventory.map;
+var db = require('../../db');
+var resultFormatter = require("../../result-formatter");
+
+const apiVersion = '1.0.0';
+
+router.get('v1/inventories/docs/efr-tb-bbt/pending', (request, response, next) => {
+    db.get().then(db => {
+       var Manager = map.get("efr-tb-bbt");
+        var manager = new Manager(db, {
+            username: 'router'
+        });
+        
+        var query = request.query;
+
+        manager.readPendingSPK(query)
+            .then(docs => { 
+                var result = resultFormatter.ok(apiVersion, 200, docs);
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+router.get('v1/inventories/docs/efr-tb-bat/pending', (request, response, next) => {
+    db.get().then(db => {
+       var Manager = map.get("efr-tb-bat");
+        var manager = new Manager(db, {
+            username: 'router'
+        });
+        var query = request.query;
+
+        manager.readPendingSPK(query)
+            .then(docs => { 
+                var result = resultFormatter.ok(apiVersion, 200, docs);
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+
+
+router.get('v1/inventories/docs/efr-tb-bbt/pending/:id', (request, response, next) => {
+    db.get().then(db => {
+       var Manager = map.get("efr-tb-bbt");
+        var manager = new Manager(db, {
+            username: 'router'
+        });
+        var id = request.params.id;
+
+        manager.getPendingSPKById(id)
+            .then(docs => { 
+                var result = resultFormatter.ok(apiVersion, 200, docs);
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+router.get('v1/inventories/docs/efr-tb-bat/pending/:id', (request, response, next) => {
+    db.get().then(db => {
+       var Manager = map.get("efr-tb-bat");
+        var manager = new Manager(db, {
+            username: 'router'
+        });
+        var id = request.params.id;
+
+        manager.getPendingSPKById(id)
+            .then(docs => { 
+                var result = resultFormatter.ok(apiVersion, 200, docs);
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+
+module.exports = router;
