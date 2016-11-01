3a0ab7f5bf21f39f279a7ac5ce12fd8b376c8504
lioenel <kepin91@yahoo.co.id>
2016-11-01 17:38:24 +0700

    remove file (#16)
    * add #dev
    
    * update read api
    
    * add new read list api
    
    * remove file
    
    * add api for report
    
    * remove pos
    
    * remove pos

--- server.js
+++ server.js
@@ -33,23 +33,5 @@ merchandiserDocModuleSpecifyRouter.applyRoutes(server, "v1/merchandiser/docs");
 var merchandiserDocModuleRouter = require('./src/routers/v1/merchandiser/merchandiser-doc-module-router');
 merchandiserDocModuleRouter.applyRoutes(server, "v1/merchandiser/docs");  
  
-var masterStoreRouter = require('./src/routers/v1/master/store-router');
-masterStoreRouter.applyRoutes(server, "v1/master/stores");
-
-var masterBankRouter = require('./src/routers/v1/master/bank-router');
-masterBankRouter.applyRoutes(server, "v1/master/banks");
-
-var masterCardTypeRouter = require('./src/routers/v1/master/card-type-router');
-masterCardTypeRouter.applyRoutes(server, "v1/master/cardtypes/"); 
-
-var salesModuleRouter = require('./src/routers/v1/sales/sales-module-router');
-salesModuleRouter.applyRoutes(server, "v1/sales/docs/sales");  
-
-var salesRewardTypeRouter = require('./src/routers/v1/sales/reward-type-router');
-salesRewardTypeRouter.applyRoutes(server, "v1/sales/rewardtypes");
-
-var salesPromoRouter = require('./src/routers/v1/sales/promo-router');
-salesPromoRouter.applyRoutes(server, "v1/sales/docs/promos"); 
-
 server.listen(process.env.PORT, process.env.IP);
 console.log(`server created at ${process.env.IP}:${process.env.PORT}`)
