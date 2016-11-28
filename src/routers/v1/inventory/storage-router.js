7124a5b309ae7714330738a936db68d353fc1835
lioenel <kepin91@yahoo.co.id>
2016-11-28 14:25:06 +0700

    update article to item and storage inventory to master

--- src/routers/v1/inventory/storage-router.js
+++ src/routers/v1/inventory/storage-router.js
@@ -1,6 +1,6 @@
 var Router = require('restify-router').Router;;
 var router = new Router();
-var StorageManager = require('bateeq-module').inventory.StorageManager;
+var StorageManager = require('bateeq-module').master.StorageManager;
 var db = require('../../../db');
 var resultFormatter = require("../../../result-formatter");
 
